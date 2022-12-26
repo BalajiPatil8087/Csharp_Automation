bd2bd180d35c2d87beaeefd1e7a55dd3a844d58b
patilbs <patilsagar80878@gmail.com>
2022-12-26 23:13:23 +0530

    SchoolManagementAssignment

---
+++ SchoolManagement/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud1 = new Student();
+            Student stud2 = new Student();
+            Student stud3 = new Student();
+
+            stud1.StudentId = 1001;
+            stud1.StudentName = "jack";
+            stud1.StudentMailID = "jack@gmail.com";
+            stud1.Percentage = 45.2;
+            Student.SchoolName = "Global school";
+            Student.SchoolAddress = "chennai";
+
+            
+
+            stud2.StudentId = 1002;
+            stud2.StudentName = "peter";
+            stud2.StudentMailID = "peter@gmail.com";
+            stud2.Percentage = 85.2;
+            Student.SchoolName = "Global school";
+            Student.SchoolAddress = "chennai";
+
+            
+
+            stud3.StudentId = 1003;
+            stud3.StudentName = "mark";
+            stud3.StudentMailID = "mark@gmail.com";
+            stud3.Percentage = 56.5;
+            Student.SchoolName = "Global school";
+            Student.SchoolAddress = "chennai";
+
+           
+
+            stud1.PrintStudentDetails();
+            stud1.DisplayCertification();
+
+            stud2.PrintStudentDetails();
+            stud2.DisplayCertification();
+
+            stud3.PrintStudentDetails();
+            stud3.DisplayCertification();
+
+
+
+        }
+    }
+}
---
+++ SchoolManagement/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement
+{
+    public class Student
+    {
+        public int StudentId;
+        public string StudentName;
+        public string StudentMailID;
+        public double Percentage;
+        public static  string SchoolName;
+        public static string SchoolAddress;
+
+     public void PrintStudentDetails()
+        {
+            Console.WriteLine("Student ID:" + StudentId);
+            Console.WriteLine("Student Name:" + StudentName);
+            Console.WriteLine("Student MailID:" + StudentMailID);
+            Console.WriteLine("Percentage:" + Percentage);
+            Console.WriteLine("SchoolName:" + Student.SchoolName);
+            Console.WriteLine("SchoolAddress:" + Student.SchoolAddress);
+        }
+        public void DisplayCertification()
+        {
+            if(Percentage>=90 && Percentage<=100)
+            {
+                Console.WriteLine("Hi " + StudentName + "You have successfully passed with grade A ");
+            }
+            else if(Percentage>=80 && Percentage <= 89)
+            {
+                Console.WriteLine("Hi " + StudentName + "You have successfully passed with grade B ");
+           }
+            else if(Percentage>=60 && Percentage <= 79)
+            {
+                Console.WriteLine("Hi " + StudentName + "You have successfully passed with grade c ");
+                
+            }
+            else
+            {
+                Console.WriteLine("Hi " + StudentName + "You have Fail this Exam ");
+            }
+
+        }
+        }
+ }
+
