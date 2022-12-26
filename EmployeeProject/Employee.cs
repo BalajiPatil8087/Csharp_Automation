82d23d640306b2530f4e6f8f0dc61861e0301d68
patilbs <patilsagar80878@gmail.com>
2022-12-26 22:26:29 +0530

    Get & Set Property

--- EmployeeProject/Employee.cs
+++ EmployeeProject/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,20 +11,70 @@ namespace EmployeeProject
     {
         public int empID;
         public string Empname;
-        public double empSalary;
+        private  double _empSalary;
         public static string companyName;
         public static string companyLocation;
+        public char empPerformanceType;
 
-        public void PrintEmployeeDetail()
+
+        public double EmpSalary
         {
-            Console.WriteLine("Employee ID: "+empID);
-            Console.WriteLine("Employee Name: " +Empname);
-            Console.WriteLine("Employee Salary: " +empSalary);
-            Console.WriteLine("Company Name "+Employee.companyName);
-            Console.WriteLine("Company Location " + Employee.companyLocation);
-
+            get { 
+                //read
+                return _empSalary; }
+            set 
+            {
+                //write
+                 if(value>3000)
+                {
+                    _empSalary = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid salary .so default value remain ");
+                }
+            }
         }
 
+       public void PrintEmployeeDetail()
+        {
+            Console.WriteLine("Employee ID: " + empID);
+            Console.WriteLine("Employee Name: " + Empname);
+            Console.WriteLine("Employee Salary: " + _empSalary);
+            Console.WriteLine("PerformanceType" + empPerformanceType);
+            Console.WriteLine("Company Name " + Employee.companyName);
+            Console.WriteLine("Company Location " + Employee.companyLocation);
 
+
+        }
+         public void GetGrossSalaryWithBonus()
+          {
+            Console.WriteLine("Employee ID: " + empID);
+            if (empPerformanceType == 'A')
+            {
+                Console.WriteLine("25 %");
+                Console.WriteLine(_empSalary + (_empSalary * 25 / 100));
+
+            }
+            else if (empPerformanceType == 'B')
+            {
+                Console.WriteLine("15 %");
+                Console.WriteLine(_empSalary + (_empSalary * 15 / 100));
+            }
+            else if (empPerformanceType == 'C')
+            {
+                Console.WriteLine("15 %");
+                Console.WriteLine(_empSalary + (_empSalary * 10 / 100));
+            }
+            else
+            {
+                Console.WriteLine("Not eligible for bonus");
+
+            }
+        }
     }
 }
+
+    
+    
+
--- EmployeeProject/Program.cs
+++ EmployeeProject/Program.cs
@@ -18,47 +18,67 @@ namespace  Maveric.Runner;
 
         emp1.empID = 101;
         emp1.Empname = "saul";
-        emp1.empSalary = 9000;
+        //emp1.empSalary = 9000;
+        emp1.empPerformanceType = 'A';
+        emp1.EmpSalary = 9000;
+
+        Console.WriteLine("******************");
 
         emp2.empID = 102;
         emp2.Empname = "kim";
-        emp2.empSalary = 12000.2;
+       // emp2.empSalary = 12000.2;
+        emp2.empPerformanceType = 'B';
+        emp2.EmpSalary = -8900.2;
+
+
+        Console.WriteLine("******************");
 
         emp3.empID = 103;
         emp3.Empname = "jack";
-        emp3.empSalary = 6000;
+        //emp3.empSalary = 6000;
+        emp3.empPerformanceType = 'C';
+        emp3.EmpSalary = 45545.65;
 
-        Employee.companyName = "Maveric compnay";
+        
 
         Console.WriteLine(emp1.empID);
         Console.WriteLine(emp1.Empname);
-        Console.WriteLine(emp1.empSalary);
+        //Console.WriteLine(emp1.empSalary);
 
         Console.WriteLine("******************");
 
         Console.WriteLine(emp2.empID);
         Console.WriteLine(emp2.Empname);
-        Console.WriteLine(emp2.empSalary);
+        //Console.WriteLine(emp2.empSalary);
 
         Console.WriteLine("******************");
         
         Console.WriteLine(emp3.empID);
         Console.WriteLine(emp3.Empname);
-        Console.WriteLine(emp3.empSalary);
+       // Console.WriteLine(emp3.empSalary);
 
 
         Console.WriteLine("******************");
 
+        emp1.PrintEmployeeDetail();
         emp2.PrintEmployeeDetail();
+        emp3.PrintEmployeeDetail();
+
+        //emp4.PrintEmployeeDetail();
 
         Console.WriteLine("******************");
 
-        emp4.PrintEmployeeDetail();
+        emp1.GetGrossSalaryWithBonus();
+        emp2.GetGrossSalaryWithBonus();
+        emp3.GetGrossSalaryWithBonus();
+        
+        Console.WriteLine("******************");
+
+        emp4.GetGrossSalaryWithBonus();
 
 
 
-        Console.WriteLine(Employee.companyName);
-        Console.WriteLine(Employee.companyLocation);
+
     }
 }
 
