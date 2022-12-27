2372760907df5d8b5c951c94daa7b650750bcb96
patilbs <patilsagar80878@gmail.com>
2022-12-27 10:42:28 +0530

    ShoppingCartProject

---
+++ ShoppingCartProject/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartProject
+{
+    public class Item
+    {
+        public int FieldID;
+        public string Descr;
+        public int Quantity;
+        public double Price;
+        public void PrintItemDetails()
+        {
+            Console.WriteLine("Field ID: " + FieldID);
+            Console.WriteLine("Description: " + Descr);
+            Console.WriteLine("Quantity: " + Quantity);
+            Console.WriteLine("Price: " + Price);
+
+        }
+        public void PrintDiscountPrice()
+        {
+            if (Quantity == 2)
+            {
+                Console.WriteLine(Price + (Price * 10 / 100));
+            }
+            else if(Quantity>=3 && Quantity <= 5)
+            {
+                Console.WriteLine(Price + (Price * 15 / 100));
+            }
+            else if (Quantity >= 5)
+            {
+                Console.WriteLine(Price + (Price * 25 / 100));
+            }
+            else
+            {
+                Console.WriteLine("No Discount");
+            }
+        }
+    }
+}
---
+++ ShoppingCartProject/ShoppingCart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartProject
+{
+    public class ShoppingCart
+    {
+        static void Main(string[] args)
+        {
+            Item item1= new Item();
+            Item item2 = new Item();
+            Item item3 = new Item();
+
+            item1.FieldID = 101;
+            item1.Descr = "Laptop";
+            item1.Quantity = 5;
+            item1.Price = 45000;
+
+
+            item2.FieldID = 102;
+            item2.Descr = "Tshirts";
+            item2.Quantity = 4;
+            item2.Price = 4500;
+
+
+            item3.FieldID = 103;
+            item3.Descr = "Mobile";
+            item3.Quantity = 1;
+            item3.Price = 45000;
+
+            item1.PrintItemDetails();
+            item1.PrintDiscountPrice();
+
+
+            item2.PrintItemDetails();
+            item2.PrintDiscountPrice();
+
+
+            item3.PrintItemDetails();
+            item3.PrintDiscountPrice();
+
+        }
+    }
+}
