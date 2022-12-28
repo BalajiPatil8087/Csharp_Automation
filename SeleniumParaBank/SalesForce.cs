78de9828211f2874bbb3ccee30f902feca90b347
patilbs <patilsagar80878@gmail.com>
2022-12-28 12:58:21 +0530

    DropDown

--- SeleniumParaBank/SalesForce.cs
+++ SeleniumParaBank/SalesForce.cs
@@ -5,13 +5,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumParaBank
 {
     public class SalesForce
     {
-        private static object element;
-
         static void Main(String[] args)
         {
             IWebDriver driver = new ChromeDriver();
@@ -20,14 +19,29 @@ namespace SeleniumParaBank
             driver.FindElement(By.Name("UserFirstName")).SendKeys("John");
             driver.FindElement(By.Name("UserLastName")).SendKeys("Wick");
             driver.FindElement(By.Name("UserEmail")).SendKeys("John@gmail.com");
-            driver.FindElement(By.Name("UserTitle")).Click();
-            driver.FindElement(By.Name("CompanyName")).SendKeys("xyz");
-            driver.FindElement(By.Name("CompanyEmployees")).Click();
-            driver.FindElement(By.Name("CompanyCountry")).Click();
+
+
+
+            SelectElement dropdown = new SelectElement(driver.FindElement(By.Name("UserTitle")));
+            dropdown.SelectByIndex(4);
+
+            driver.FindElement(By.Name("CompanyName")).SendKeys("Maveric");
+
+
+
+            SelectElement dropdown1 = new SelectElement(driver.FindElement(By.Name("CompanyEmployees")));
+            dropdown1.SelectByIndex(3);
+
+            driver.FindElement(By.Name("UserPhone")).SendKeys("");
+
+            SelectElement dropdown4 = new SelectElement(driver.FindElement(By.Name("CompanyCountry")));
+            dropdown4.SelectByValue("United Kingdom");
+
             driver.FindElement(By.ClassName("checkbox-ui")).Click();
+
             driver.FindElement(By.Name("start my free trial")).Click();
 
-            
+
 
         }
     }
