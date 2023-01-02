d4e67438f51354af1773de29fed9d45b18ce0d56
patilbs <patilsagar80878@gmail.com>
2023-01-02 14:58:56 +0530

    selenium Assignment

--- SeleniumAdvance/Citi.cs
+++ SeleniumAdvance/Citi.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using OpenQA.Selenium.Support.Extensions;
 
 namespace SeleniumAdvance
 {
@@ -16,66 +17,33 @@ namespace SeleniumAdvance
         public void CitiBank()
         {
             IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
             driver.Url = "https://www.online.citibank.co.in/";
-
-            driver.Manage().Window.Maximize();
-            Thread.Sleep(1000);
-
-            driver.FindElement(By.XPath("//a[@class='fancybox-item fancybox-close']")).Click();
-
-            driver.FindElement(By.XPath("(//img[@title='LOGIN NOW'])[1]")).Click();
-            Thread.Sleep(1000);
-
-
-
+            driver.FindElement(By.XPath("//a[@title='Close']")).Click();
+            driver.FindElement(By.XPath("//span[@class='txtSign']")).Click();
             driver.SwitchTo().Window(driver.WindowHandles[1]);
-
-            Thread.Sleep(1000);
-
             driver.FindElement(By.XPath("//div[@onclick='ForgotUserID();']")).Click();
+            driver.FindElement(By.LinkText("select your product type")).Click();
+            driver.FindElement(By.LinkText("Credit Card")).Click();
+            driver.FindElement(By.Id("citiCard1")).SendKeys("4545");
+            driver.FindElement(By.Id("citiCard2")).SendKeys("5656");
+            driver.FindElement(By.Id("citiCard3")).SendKeys("8887");
+            driver.FindElement(By.Id("citiCard4")).SendKeys("9998");
+            driver.FindElement(By.Id("cvvnumber")).SendKeys("234");
+            driver.FindElement(By.Id("bill-date-long")).Click();
+            SelectElement month = new SelectElement(driver.FindElement(By.XPath("//select[@class='ui-datepicker-month']")));
+            month.SelectByText("Apr");
+            SelectElement year = new SelectElement(driver.FindElement(By.XPath("//select[@class='ui-datepicker-year']")));
+            year.SelectByText("2022");
+            driver.FindElement(By.LinkText("14")).Click();
+            driver.FindElement(By.XPath("//input[@type='button']")).Click();
             Thread.Sleep(1000);
-            driver.FindElement(By.XPath("(//a[text()='select your product type'])[1]")).Click();
-            Thread.Sleep(1000);
-           driver.FindElement(By.XPath("//a[text()='Credit Card']")).Click();
-          //  select.SelectByText("Credit Card");
+            string textPrint = driver.FindElement(By.XPath("//li[contains(text(),'• Please accept Terms and Conditions ')]")).Text;
+            Console.WriteLine(textPrint);
 
-            driver.FindElement(By.Name("citiCard1")).SendKeys("4545");
-            driver.FindElement(By.Name("citiCard2")).SendKeys("5656");
-            driver.FindElement(By.Name("citiCard3")).SendKeys("1254");
-            driver.FindElement(By.Name("citiCard4")).SendKeys("9996");
 
-            //driver.FindElement(By.Name("CCVNO")).SendKeys("510");
 
-            driver.FindElement(By.XPath("//input[@onkeypress='return numbersonly(event)']")).SendKeys("456");
-
-            driver.FindElement(By.Name("DOB")).Click();
-
-            driver.FindElement(By.XPath("(//select[@data-event='change'])[1]")).Click();
-
-           // SelectElement s = new SelectElement(driver.FindElement(By.XPath("//select[@class='ui-datepicker-month']")));
-            //s.SelectByValue("Apr");
-
-            driver.FindElement(By.XPath("//option[text()='Apr']")).Click();
-
-            driver.FindElement(By.XPath("//a[text()='14']")).Click();
-
-            //.driver.FindElement(By.Name("DOB")).SendKeys("4/04/2022");
-
-            //driver.FindElement(By.Id("agree")).Click();
-
-            driver.FindElement(By.XPath("//input[@value='PROCEED']")).Click();
-
-            // string text = driver.FindElement(By.XPath("//p[contains(text(),'We are unable to process your request. Please try again later.')]")).Text;
-            //Console.WriteLine(text);
-
-            //driver.SwitchTo().Window(driver.WindowHandles[2]);
-
-            string s=driver.FindElement(By.XPath("(//li[contains(text(),'• Please accept Terms and Conditions ')])[3]")).Text;
-            Console.WriteLine(s);
-            //  driver.SwitchTo().Window(driver.WindowHandles[0]);
-            // Console.WriteLine(driver.Title);
-            // driver.Quit();
 
         }
     }
---
+++ SeleniumAdvance/FrameTest.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SeleniumAdvance
+{
+    public class FrameTest
+    {
+        [Test]
+      public void Demo1Frame()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Url = "https://netbanking.hdfcbank.com/netbanking/";
+
+            
+            driver.SwitchTo().Frame(driver.FindElement(By.XPath("//frame[@name='login_page']")));
+            
+            driver.FindElement(By.Name("fldLoginUserId")).SendKeys("kevin123");
+            
+            driver.FindElement(By.LinkText("CONTINUE")).Click();
+
+        }
+        [Test]
+        public void Demo2Frame()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Url = "https://netbanking.hdfcbank.com/netbanking/IpinResetUsingOTP.htm";
+
+            driver.FindElement(By.XPath("//img[@alt='Go']")).Click();
+         
+            string alert=  driver.SwitchTo().Alert().Text;
+            Console.WriteLine(alert);
+           
+            driver.SwitchTo().Alert().Accept();
+
+           
+
+        }
+    }
+}
